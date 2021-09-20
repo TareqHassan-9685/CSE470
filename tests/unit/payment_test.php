fe950e92861acb5c23d396e85ecada1e922ca315
TareqHasssan-9685 <89999744+TareqHassan-9685@users.noreply.github.com>
2021-09-21 03:21:11 +0600

    Add files via upload

---
+++ tests/unit/payment_test.php
@@ -0,0 +1,27 @@
+<html>
+
+<body>
+<?php
+    $conn = new mysqli("localhost", "root", "", "hotel");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * from payment";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_row($result);
+	
+	while ($queryRow = $result->fetch_row()) {
+    	echo " ";
+    	for($i = 0; $i < $result->field_count; $i++){
+        	echo " $queryRow[$i] ";
+    }
+    echo "  ";
+}
+
+	echo implode(" ",$row);
+	?>
+
+	
+</body>
+
+</html>
